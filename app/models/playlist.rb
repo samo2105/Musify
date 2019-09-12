ae35ccc6102d6e35d84041c23cd631678b53c663
Samuel <samuel.cea95@gmail.com>
2019-09-12 12:16:17 -0300

    Song Model, Playslists form asociated with user, changed playlists views with user.name

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,6 @@
 class Playlist < ApplicationRecord
   belongs_to :user
+  has_many :songs
 
   validates :name, uniqueness: true, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_141131) do
+ActiveRecord::Schema.define(version: 2019_09_12_151223) do
 
   create_table "playlists", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2019_09_12_141131) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_playlists_on_user_id"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "artist"
+    t.string "name"
+    t.integer "playlist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["playlist_id"], name: "index_songs_on_playlist_id"
   end
 
   create_table "users", force: :cascade do |t|
